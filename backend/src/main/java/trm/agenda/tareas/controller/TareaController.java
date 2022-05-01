efcfcc70c519adb2f5b7d716884d1faa84984b67
trm <trms2702@gmail.com>
2022-05-01 18:14:34 +0200

    Añadir endpoint de busqueda por id
    closes #10

--- backend/src/main/java/trm/agenda/tareas/controller/TareaController.java
+++ backend/src/main/java/trm/agenda/tareas/controller/TareaController.java
@@ -1,6 +1,8 @@
 package trm.agenda.tareas.controller;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import javax.persistence.Entity;
 import javax.validation.Valid;
@@ -8,11 +10,13 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import trm.agenda.response.exception.EntityNotFoundException;
 import trm.agenda.tareas.domain.model.Tarea;
 import trm.agenda.tareas.domain.repository.TareaRepository;
 
@@ -32,6 +36,14 @@ public class TareaController {
     public ResponseEntity<Tarea> newTask(@Valid @RequestBody Tarea task) {
         this.tareaRepository.save(task);
         return ResponseEntity.ok(task);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tarea> findById(@PathVariable UUID id) {
+        // Almacenamamos en tarea
+        Optional<Tarea> tarea = this.tareaRepository.findById(id);
+        tarea.orElseThrow(() -> new EntityNotFoundException(id, Tarea.class));
+        return ResponseEntity.ok(tarea.get());
     }
 
 }
