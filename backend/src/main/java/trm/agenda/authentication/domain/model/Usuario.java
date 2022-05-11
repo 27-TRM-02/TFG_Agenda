4fb4f67b84ca8aa6083af6f87dd5d52003e6c81b
trm <trms2702@gmail.com>
2022-05-11 20:38:44 +0200

    Campo usurname añadido
    Closes #43

--- backend/src/main/java/trm/agenda/authentication/domain/model/Usuario.java
+++ backend/src/main/java/trm/agenda/authentication/domain/model/Usuario.java
@@ -2,9 +2,13 @@ package trm.agenda.authentication.domain.model;
 
 import java.util.UUID;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.Type;
 
@@ -17,8 +21,20 @@ public class Usuario {
     @GeneratedValue
     private UUID id;
 
+    // Declaracion de campo usurname
+    @NotNull
+    @Column(nullable = false, length = 20)
+    @Size(min = 8, max = 20)
+    @Pattern(regexp = "^([a-zA-Z0-9_]){8,20}$")
+    private String usurname;
+
     // Constructor de la clase vacio para que no salte excepcion
     public Usuario() {
+
+    }
+
+    public Usuario(String usurname) {
+        this.setUsurname(usurname);
 
     }
 
@@ -29,6 +45,16 @@ public class Usuario {
 
     public Usuario setId(UUID id) {
         this.id = id;
+        return this;
+    }
+
+    // Constructores de campo usurname
+    public String getUsurname() {
+        return usurname;
+    }
+
+    public Usuario setUsurname(String usurname) {
+        this.usurname = usurname;
         return this;
     }
 
